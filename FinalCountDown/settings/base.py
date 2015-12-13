d6efd60df544519939fbc4d06d76a1744f7a1c45
mtumlin <mattias.tumlin@gmail.com>
2015-12-13 19:52:15 +0100

    commit

--- FinalCountDown/settings/base.py
+++ FinalCountDown/settings/base.py
@@ -45,6 +45,6 @@ DATABASES ={}
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
